57e34ca4c8628ab8c6630b4ca4d99d73390a97e0
ngphuan2017 <phuan2017@gmail.com>
2024-04-06 16:59:51 +0700

    feat: version 1.2.2

--- src/main/webapp/resources/js/notification.js
+++ src/main/webapp/resources/js/notification.js
@@ -14,3 +14,20 @@ function copyCode(id) {
         Swal.fire("Sao chép thất bại. Hãy thử lại!", "", "error");
     });
 }
+
+function copyWheelCode(id) {
+    const textToCopy = document.getElementById(`wheel-code-${id}`);
+
+    const clipboard = new ClipboardJS('.copy-button', {
+        text: () => textToCopy
+    });
+
+    clipboard.on('success', (e) => {
+        e.clearSelection();
+        Swal.fire("Đã sao chép!", "", "success");
+    });
+
+    clipboard.on('error', () => {
+        Swal.fire("Sao chép thất bại. Hãy thử lại!", "", "error");
+    });
+}
